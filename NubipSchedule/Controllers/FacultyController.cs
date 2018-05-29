c5452d91d237b13a06a017fc9ce672b605cbc27b
Artemchuk Dmytro <taruuuch@gmail.com>
2018-05-29 23:24:10 +0300

    Add default controllers for api

---
+++ NubipSchedule/Controllers/FacultyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NubipSchedule.Controllers
+{
+    [Route("api/[controller]")]
+    public class FacultyController : Controller
+    {
+        [HttpGet]
+        public List<object> Get()
+        {
+            List<object> list = new List<object>();
+            
+            list.Add("Інформаційних технологій");
+            list.Add("Аграрного менеджменту");
+            list.Add("Агробіологічний");
+            list.Add("Ветеринарної медицини");
+            list.Add("Економічний");
+
+            return list;
+        }
+    }
+}
---
+++ NubipSchedule/Controllers/GroupController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NubipSchedule.Controllers
+{
+    [Route("api/[controller]")]
+    public class GroupController : Controller
+    {
+        [HttpGet]
+        public string GetAll()
+        {
+            return "group";
+        }
+        
+        [HttpGet("{id}")]
+        public string GetLesson(int id)
+        {
+            return "group #" + id;
+        }
+    }
+}
